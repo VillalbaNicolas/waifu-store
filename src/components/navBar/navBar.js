d69c2ce70a4153074d9670c5acb17b88c5b4802a
Nicolas Villalba <nvillalba@adistec.com>
2022-08-16 14:44:45 -0300

    feat(002) add nav-bar

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+    <nav>
+        <li>Home</li>
+        <li>Store</li>
+        <li>About Us</li>
+        <li>Contact</li>
+    </nav>
+    )
+}
+
+
+export default NavBar;
