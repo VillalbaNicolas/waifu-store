d4bc2b797c0e7e21bf70cc0bd137eda728c61ad1
Nicolas Villalba <nvillalba@adistec.com>
2022-08-24 17:51:32 -0300

    feat(003) Agregadi de carrito

---
+++ src/components/navBar/cartWidget.js
@@ -0,0 +1,12 @@
+import { BsBasket3 } from "react-icons/bs";
+import "./cart.css"
+
+const CartWidget = () => {
+
+    return(
+       <BsBasket3 className="Cart"/>
+    )
+}
+
+
+export default CartWidget;
