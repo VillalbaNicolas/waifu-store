7817fa716b94a60461643f9241ebaebbc114a19f
Nicolas Villalba <nvillalba@adistec.com>
2022-09-07 16:26:41 -0300

    feat(008) Se agregan detalles de producto al ONCLICK

--- src/components/itemList/item.js
+++ src/components/itemList/item.js
@@ -1,10 +1,21 @@
+import {useState} from 'react';
+
 const Item = ({item}) => {
+    const [isActive, setIsActive] = useState(false);
+    console.log(item);
+    const handleClick = event => {
+        setIsActive(current => !current);
+      };
     return(
-    <li id = {item.id}>
+    <li id = {item.id}  className={isActive ? 'activeList' : ''} onClick={handleClick}>
         <div>
             <img src= {item.pictureUrl} alt=""/>
             <p>{item.title}</p>
-            <p>${item.price}</p>
+            <div className={isActive ? 'activeDetails' : 'disableDetails'}>
+                <p>{item.description}</p>
+                <p>{item.title}</p>
+                <p>${item.price}</p>
+            </div>
         </div>
     </li>
     )
